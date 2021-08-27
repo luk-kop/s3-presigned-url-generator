e294fca3b1e11fb8b77ae85faf1621eaff4d835d
luk-kop <luk.kopicki@gmail.com>
2021-08-27 17:32:39 +0200

    Added check_object func (verify the object existence and permissions).

--- main.py
+++ main.py
@@ -23,6 +23,29 @@ def check_bucket(bucket_name: str) -> bool:
     return False
 
 
+def check_object(bucket_name: str, object_key: str) -> bool:
+    """
+    Determine whether a object exists and you have permission to access it.
+    """
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.head_object(
+            Bucket=bucket_name,
+            Key=object_key
+        )
+        return True
+    except ClientError as error:
+        print(error)
+        error_code = error.response['Error']['Code']
+        if error_code == '404':
+            print(f'Object {object_key} does not exist')
+        elif error_code == '403':
+            print(f'You do not have permission to access {object_key} object')
+        else:
+            print('An error has occurred')
+    return False
+
+
 def create_presigned_url(bucket_name: str, object_name:str , expiration: str = 3600) -> Union[str, None]:
     """
     Generate a presigned URL to share an S3 object
@@ -48,7 +71,8 @@ if __name__ == '__main__':
 
     # Check whether bucket exist and authenticated user has permissions to access it
     bucket_exist = check_bucket(bucket_name=s3_bucket_name)
+    object_exist = check_object(bucket_name=s3_bucket_name, object_key=s3_object_name)
 
-    if bucket_exist:
+    if bucket_exist and object_exist:
         url = create_presigned_url(bucket_name=s3_bucket_name, object_name=s3_object_name)
         print(f'URL = {url}')
