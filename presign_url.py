ab3e353531d2199ddbb04692331213b6e23155c7
luk-kop <luk.kopicki@gmail.com>
2021-09-01 12:51:54 +0200

    Added argparse feature.

--- presign_url.py
+++ presign_url.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+
 from typing import Union
 from datetime import datetime, timedelta
+import argparse
 
 import boto3
 from botocore.exceptions import ClientError
@@ -31,11 +34,11 @@ def check_s3_data(bucket_name: str, object_key: str) -> bool:
         error_code = error.response['Error']['Code']
         item_type, item_name = checked_item['type'], checked_item['name']
         if error_code == '404':
-            print(f'{item_type.capitalize()} {item_name} does not exist')
+            print(f'Error: {item_type.capitalize()} {item_name} does not exist')
         elif error_code == '403':
-            print(f'You do not have permission to access {item_name} {item_type}')
+            print(f'Error: You do not have permission to access {item_name} {item_type}')
         else:
-            print('An error has occurred')
+            print('Error: An error has occurred')
     return False
 
 
@@ -62,19 +65,28 @@ def create_presigned_url(bucket_name: str, object_name:str, expiration_time: str
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='The script generates S3 object presigned URL')
+    parser.add_argument('-b', '--bucket', required=True, help='S3 bucket name')
+    parser.add_argument('-o', '--object', required=True, help='S3 object key')
+    parser.add_argument('-e', '--expires',
+                        type=int,
+                        default=60,
+                        help='Number of minutes until the pre-signed URL expires (default 60 min)')
+    args = parser.parse_args()
 
-    s3_bucket_name = 'test-pre-bucket'
-    s3_object_name = 'test-object'
-    # Expiration time in minutes
-    url_expiration_time = 5
+    # Get data from argparse
+    s3_bucket_name = args.bucket
+    s3_object_key = args.object
+    # Expiration time in minutes - default 60 minutes
+    url_expiration_time = args.expires
 
     # Check whether bucket and object exist and authenticated user has permissions to access them.
     s3_data_correct = check_s3_data(bucket_name=s3_bucket_name,
-                                    object_key=s3_object_name)
+                                    object_key=s3_object_key)
 
     if s3_data_correct:
         url = create_presigned_url(bucket_name=s3_bucket_name,
-                                   object_name=s3_object_name,
+                                   object_name=s3_object_key,
                                    expiration_time=url_expiration_time)
         valid_time = datetime.now() + timedelta(minutes=url_expiration_time)
         # Time in format: 28-Aug-2021 06:03:46
